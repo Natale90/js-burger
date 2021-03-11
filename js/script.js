785f20c18ed2c898ecd6aafe0e319090c0058341
Natale Capristo <andreacapristo1@gmail.com>
2021-03-11 16:28:38 +0100

    Just started new JS exercise

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var calculateBtn = document.getElementById("calculate");
+
+
+
+
+var burgerName = document.getElementById("burger_name").value;
+calculateBtn.addEventListener("click", function(){
+
+  if(burgerName < 1){
+
+    alert("Inserisci il nome del panino");
+  } else {
+
+    
+  }
+
+
+})
