0acb5dd053c2314203ba9dabb6249015f371b75f
Natale Capristo <andreacapristo1@gmail.com>
2021-03-11 19:07:12 +0100

    Create you burger js done

--- js/script.js
+++ js/script.js
@@ -1,18 +1,55 @@
 var calculateBtn = document.getElementById("calculate");
 
 
-
-
 var burgerName = document.getElementById("burger_name").value;
 calculateBtn.addEventListener("click", function(){
 
-  if(burgerName < 1){
+  if(burgerName.length < 1){
 
     alert("Inserisci il nome del panino");
   } else {
 
-    
+    var prezzoFinale = 10;
+
+    var ingredientsList = document.getElementsByClassName("Ingredients");
+    // console.log(ingredientsList);
+    for (var i = 0; i < ingredientsList.length; i++){
+
+      var listItem = ingredientsList[i];
+      var ischecked = listItem.checked;
+      var price = parseInt(listItem.dataset.price);
+      console.log(listItem, ischecked);
+
+      if (ischecked){
+
+        prezzoFinale += price;
+      }
+
+
+
+    }
+
+    var clietCoupon = document.getElementById("burger-coupon").value;
+    console.log(clietCoupon);
+
+    var coupons = [
+      "asdtre",
+      "cgftrn",
+      "mnblkj"
+    ];
+
+    for (i = 0; i < coupons.length; i++){
+
+      var couponAccepted = coupons[i];
+    }
+
+    if(couponAccepted == clietCoupon){
+
+      prezzoFinale = prezzoFinale - prezzoFinale * 0.2;
+    }
   }
 
+  document.getElementById("prezzo").innerHTML = prezzoFinale;
+  console.log(listItem, ischecked, prezzoFinale);
 
 })
