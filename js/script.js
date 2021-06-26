70ceca3d6d4655c3cf7c2bbba8798c96bfdba7af
Natale Capristo <andreacapristo1@gmail.com>
2021-06-26 21:59:03 +0530

    refactoring 1

--- js/script.js
+++ js/script.js
@@ -1,73 +1,71 @@
-var calculateBtn = document.getElementById("calculate");
 
+//aggancio il bottone "calculate_botton" dell'html alla variabile "calculateBtn" in js
+var calculateBtn = document.getElementById("calculate_button");
 
-var burgerName = document.getElementById("burger_name").value;
+var coupons = ["coupon1", "coupon2", "coupon3", "coupon4"];
+var finalPrice;
+var ingredients = document.getElementsByClassName("column");
 calculateBtn.addEventListener("click", function(){
 
-  if(burgerName.length < 1){
+  //estraggo il valore dell'input "burger_name" dall'html".
+  var burgerName = document.getElementById("burger_name").value;
 
-    alert("Inserisci il nome del panino");
+  if (burgerName.length < 1){
+
+    alert("Inserire il nome del panino")
   } else {
 
-    var prezzoFinale = 10;
+    finalPrice = 10;
+    var checkboxes = document.getElementsByClassName("ingredients");
 
-    var ingredientsList = document.getElementsByClassName("Ingredients");
-    // console.log(ingredientsList);
-    for (var i = 0; i < ingredientsList.length; i++){
+    for (var i = 0; i < checkboxes.length; i++){
 
-      var listItem = ingredientsList[i];
-      var ischecked = listItem.checked;
-      var price = parseInt(listItem.dataset.price);
-      console.log(listItem, ischecked);
+      var singolCheckBox = checkboxes[i];
+      var isChecked = singolCheckBox.checked;
 
-      if (ischecked){
+      if (isChecked){
 
-        prezzoFinale += price;
-      }
+        var price = parseInt(singolCheckBox.dataset.price);
+        finalPrice += price;
 
-
-
-    }
-
-
-    var coupons = [
-      "asdtre",
-      "cgftrn",
-      "mnblkj"
-    ];
-
-    var clietCoupon = document.getElementById("burger-coupon").value;
-    console.log(clietCoupon);
-    for (i = 0; i < coupons.length; i++){
-
-      var couponAccepted = coupons[i];
-      if(couponAccepted == clietCoupon){
-
-        prezzoFinale = prezzoFinale - prezzoFinale * 0.2;
       }
     }
+
+    var clietCoupon = document.getElementById("coupon_code").value;
+
+    if (coupons.includes(clietCoupon)){
+
+      finalPrice = finalPrice - (finalPrice * 20 / 100);
+    }
+
 
   }
 
-  document.getElementById("prezzo").innerHTML = prezzoFinale;
-  console.log(listItem, ischecked, prezzoFinale);
+  var spanPrice = document.getElementById("prezzo");
+  spanPrice.innerHTML = finalPrice;
 
 })
 
-var lis = document.getElementsByTagName("li");
-console.log(lis);
+for (var i = 0; i < ingredients.length;i++){
 
-for(i = 0; i < lis.length; i++){
+    var ingredient = ingredients[i];
 
-  var li = lis[i];
+    ingredient.addEventListener('click', function(){
 
-  li.addEventListener("click", function(){
+      var item = (this);
+      var input = item.children[1];
 
-    var clicked = this;
-    var clickedChildrens = clicked.children;
-    var checkbox = clickedChildrens[1];
-    checkbox.checked = !checkbox.checked;
-    
-    console.log(this.children[1], clickedChildrens[1]);
-  })
+      input.checked = !input.checked;
+
+      // ----- still don't know how to show the fake check------------
+      var fakeCheck = document.getElementsByClassName('fake_check');
+      if(input.checked){
+
+        fakeCheck.addClass('fake_check_active');
+      } else {
+
+        fake_check.removeClass('fake_check_active')
+      }
+      // --------------------------------------------------------------
+    })
 }
